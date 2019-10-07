294416870e964a04fc2b4d364f7d25165359af92
156789qw <156789qw@gmail.com>
2019-10-07 21:28:59 +0300

    kendo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { products } from "./products";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.sass"]
 })
 export class AppComponent {
-  title = 'ngKendoGrid';
+  title = "ngKendoGrid";
+  public gridData: any[] = products;
 }
